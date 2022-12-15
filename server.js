4e479763da98eb9b885e48a1c5f61196921e6919
EmmanuelU <emmanuelurias60@icloud.com>
2022-12-14 21:13:34 -0800

    added files

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
-const express =require('express')
+//dependencies
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-const app = express()
+//configs
 require('dotenv').config()
-const PORT = process.env.PORT
+PORT = process.env.PORT
+const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+var corsOptions = {
+    origin: 'http://localhost:3000.com',
+    optionsSuccessStatus: 200
+  }
 
+// MIDDLEWARE
+app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
+//routes
 app.get('/', (req, res) => {
-    res.send('hello world')
+    res.send('Heyyy')
 })
 
-app.listen(PORT, () => console.log(`server is running`))
+//app.get('*', (req, res) => {
+//    res.send('error404')
+//})
 
-module.exports = app;
+// books: 
+const booksController = require('./controllers/books-controller.js')
+app.use('/books', booksController)
+
+//listen
+app.listen(PORT, () => {
+    console.log('Greetings! From port: ', PORT);
+  })
