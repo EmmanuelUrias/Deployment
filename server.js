826469fbd50fbf4277dd6d5394f1d24827f246d7
EmmanuelU <emmanuelurias60@icloud.com>
2022-12-14 19:06:22 -0800

    updated environment

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express =require('express')
 
 const app = express()
 require('dotenv').config()
+const PORT = process.env.PORT
 
 
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
-app.listen(3000)
+app.listen(PORT, () => console.log(`server is running`))
 
 module.exports = app;
