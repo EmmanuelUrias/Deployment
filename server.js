008e1a1521897e64014cca0b4bdcb66c6b303eb0
EmmanuelU <emmanuelurias60@icloud.com>
2022-12-14 18:51:03 -0800

    vercel deployment support

--- server.js
+++ server.js
@@ -2,3 +2,12 @@ const express =require('express')
 
 const app = express()
 require('dotenv').config()
+
+
+app.get('/', (req, res) => {
+    res.send('hello world')
+})
+
+app.listen(3000)
+
+module.exports = app;
